837d505e53d9e7b49d2ae524838df5bfa7ce4e16
ewoutvdb <vdbewout@gmail.com>
2022-11-15 18:01:08 +0100

    only target android

--- Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace c_sherp_project_app;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
