e5c6d798759a4672e24a649acffef23f1f787edb
ewoutvdb <vdbewout@gmail.com>
2022-11-22 12:03:21 +0000

    buttons

--- PlayPage.xaml.cs
+++ PlayPage.xaml.cs
@@ -6,5 +6,21 @@ public partial class PlayPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void OnRockClicked(object sender, EventArgs e)
+	{
+		System.Console.WriteLine("Rock Clicked");
+
+	}
+
+	private void OnPaperClicked(object sender, EventArgs e)
+	{
+		System.Console.WriteLine("Paper Clicked");
+	}
+
+	private void OnScissorsClicked(object sender, EventArgs e)
+	{
+		System.Console.WriteLine("Scissors Clicked");
+	}
 }
 
